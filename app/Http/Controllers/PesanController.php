db18d84ebeebddcc7b9e3ea5bcf96cabdfa950aa
ankhy09 <ankhy09@gmail.com>
2020-12-01 10:42:11 +0700

    sidang desember

--- app/Http/Controllers/PesanController.php
+++ app/Http/Controllers/PesanController.php
@@ -14,10 +14,10 @@ use Illuminate\Http\Request;
 
 class PesanController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index($id)
     {
